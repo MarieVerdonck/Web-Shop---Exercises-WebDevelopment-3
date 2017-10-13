d02ac80657ff08279a5ce13a558d37ba31101b70
Marie <maria.verdonck@student.ucll.be>
2017-10-13 12:14:56 +0200

    moved seleniumWorksWellTest (not really testing application, just whether selenium works)

---
+++ Web3_Project/test/SeleniumWorksWellTest.java
@@ -0,0 +1,38 @@
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import view.SeleniumTest;
+
+public class SeleniumWorksWellTest extends SeleniumTest {
+
+	@Before
+	public void setUpTest() {
+		driver.get("https://nl.wikipedia.org/wiki/Hoofdpagina");
+	}
+
+	@Test
+	public void browserVindtWikipedia() {
+		assertEquals("Wikipedia, de vrije encyclopedie", driver.getTitle());
+	}
+
+	@Test
+	public void wikipediaVindtSelenium() {
+		WebElement field = driver.findElement(By.id("searchInput"));
+		field.clear();
+		field.sendKeys("selenium");
+		WebElement link = driver.findElement(By.id("searchButton"));
+		link.click();
+
+		assertEquals("Selenium - Wikipedia", driver.getTitle());
+
+		assertEquals("Selenium", driver.findElement(By.tagName("h1")).getText());
+
+	}
+
+}
